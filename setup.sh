2d8869e08111ced8d5c60a86b4952e705b3c6f92
Gabriele <gabriele.s@21gear.com>
2015-11-13 11:00:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install python-matplotlib
 echo Installing numpy...
 sudo apt-get install python-numpy
 echo Done.
-echo "Check if pip is installed..."
+echo Installing python-pip...
 echo "deb http://archive.ubuntu.com/ubuntu/ vivid universe" | sudo tee -a "/etc/apt/sources.list"
 sudo apt-get install python-pip
 echo "Installing geojson python module for GIS support..."
