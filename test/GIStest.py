8f4b8d3f4ba1be842e763d8fa23af4eb9a2eb859
Gabriele <gabriele.s@21gear.com>
2015-11-13 11:33:04 +0100

    Added example GIS json file to execute tests. Added test main script for GIS functionality. Added library on tredify file

---
+++ test/GIStest.py
@@ -0,0 +1,21 @@
+import os 
+import sys 
+import string
+import argparse
+
+sys.path.insert(0, '../lib/')
+from Parser import *
+from Polygon import *
+from Scatter import *
+from Pie import *
+from Bar import *
+from GIS import *
+
+
+if __name__ == '__main__':
+	
+	parse_GIS = Parser('../json/geojson.json')
+	dict_gis = parse_GIS.load_json()
+
+	print dict_gis
+ 
